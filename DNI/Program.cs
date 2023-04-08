650b5781a6ff0fe43bdc3dc0402ff196a31ae962
madaww <115369235+madaww@users.noreply.github.com>
2023-04-08 23:39:38 +0200

    subida tarea alberto

---
+++ DNI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DNI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your DNI/NIF number (Without the letter):");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your complete DNI/NIF is: " + number + LetterNIF(number));
+        }
+
+        /// <summary>
+        /// This 
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static char LetterNIF(int number)
+        {
+            String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+            int resto = number % 23;
+            char caracter = letras[resto];
+            return caracter;
+        }
+    }
+}
